185ef14dc70afa20e74287a514e0d1c314af31bf
slyo <sean.lyo@outlook.com>
2020-12-28 17:36:46 +0800

    feat(week-of-year): Add date range for week.

--- week-of-year/src/main.rs
+++ week-of-year/src/main.rs
@@ -1,28 +1,36 @@
 extern crate chrono;
 
 use std::error::Error;
-use std::string::ToString;
 
 use chrono::naive::NaiveDate;
 use chrono::offset::Local;
-use chrono::{Date, Datelike, Weekday};
+use chrono::Duration;
+use chrono::{Date, Datelike};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let date: Date<Local> = Local::today();
-    let padded: bool = matches!(
-        Date::<Local>::from_utc(NaiveDate::from_ymd(date.year(), 1, 1), *date.offset()).weekday(),
-        Weekday::Mon
-    );
-    let woy = {
-        let weeks = date.format("%j").to_string().parse::<f32>()? / 7.0;
-        if weeks.fract() > 0.0 {
-            Ok::<u32, Box<dyn Error>>(weeks.ceil() as u32)
-        } else if !padded {
-            Ok(weeks.trunc() as u32 + 1)
+    let padding = date.weekday().num_days_from_monday();
+    let start = {
+        let tmp = date - Duration::days(padding as i64);
+        if tmp.year() != date.year() {
+            Date::<Local>::from_utc(NaiveDate::from_ymd(date.year(), 1, 1), *date.offset())
         } else {
-            Ok(weeks.trunc() as u32)
+            tmp
         }
-    }?;
-    println!("Week of this year: {}", woy);
+    };
+    let end = {
+        let tmp = date + Duration::days((6 - padding) as i64);
+        if tmp.year() != date.year() {
+            tmp - Duration::days(tmp.day() as i64)
+        } else {
+            tmp
+        }
+    };
+    println!(
+        "Week {}: {}-{}",
+        date.format("%W").to_string().parse::<u32>()? + 1,
+        start.format("%Y/%m/%d"),
+        end.format("%Y/%m/%d")
+    );
     Ok(())
 }
