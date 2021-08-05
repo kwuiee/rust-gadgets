834f4c32f6681e380a7ef45a8dc2c5ffd2c95920
slyo <sean.lyo@outlook.com>
2021-08-05 23:47:15 +0800

    tsv-split: Add optional line number limit.

--- tsv-split/src/main.rs
+++ tsv-split/src/main.rs
@@ -3,17 +3,91 @@ extern crate clap;
 extern crate csv;
 
 use std::collections::{BTreeMap, HashMap};
-use std::error::Error;
 use std::ffi::OsStr;
 use std::fs::File;
+use std::io;
 use std::path::PathBuf;
 
 use clap::{AppSettings, Clap};
 use csv::ReaderBuilder as CsvReaderBuilder;
+use csv::StringRecord;
 use csv::Writer as CsvWriter;
 use csv::WriterBuilder as CsvWriterBuilder;
 
+const BUFFER: usize = 1 << 20;
 type Row = BTreeMap<String, String>;
+
+struct WriterGroup<'a, 'b, 'c> {
+    prefix: &'a str,
+    ext: &'b str,
+    headers: &'c StringRecord,
+    limit: Option<u32>,
+    inner: HashMap<String, (CsvWriter<File>, u32, u32)>,
+}
+
+impl<'a, 'b, 'c> WriterGroup<'a, 'b, 'c> {
+    fn new(prefix: &'a str, ext: &'b str, headers: &'c StringRecord, limit: Option<u32>) -> Self {
+        Self {
+            prefix,
+            ext,
+            headers,
+            limit,
+            inner: HashMap::new(),
+        }
+    }
+
+    fn write(&mut self, part: String, rec: &Vec<&String>) -> Result<(), io::Error> {
+        // If line number reached limit
+        match (self.limit, self.inner.get_mut(&part)) {
+            (Some(num), Some(col)) => {
+                if num <= col.1 {
+                    col.2 += 1;
+                    col.0 = CsvWriterBuilder::new()
+                        .delimiter(b'\t')
+                        .has_headers(true)
+                        .buffer_capacity(BUFFER)
+                        .from_path(format!("{}.{}.{}.{}", self.prefix, &part, col.2, self.ext))?;
+                    col.0.write_record(self.headers)?;
+                    col.1 = 1;
+                };
+                col.0.write_record(rec)?;
+                col.1 += 1;
+            }
+            (Some(_), None) => {
+                let tag = 0;
+                let mut wtr = CsvWriterBuilder::new()
+                    .delimiter(b'\t')
+                    .has_headers(true)
+                    .buffer_capacity(BUFFER)
+                    .from_path(format!("{}.{}.{}.{}", self.prefix, &part, tag, self.ext))?;
+                wtr.write_record(self.headers)?;
+                wtr.write_record(rec)?;
+                self.inner.entry(part).or_insert((wtr, 2, tag));
+            }
+            (None, Some((wtr, _, _))) => {
+                wtr.write_record(rec)?;
+            }
+            (None, None) => {
+                let mut wtr = CsvWriterBuilder::new()
+                    .delimiter(b'\t')
+                    .has_headers(true)
+                    .buffer_capacity(BUFFER)
+                    .from_path(format!("{}.{}.{}", self.prefix, &part, self.ext))?;
+                wtr.write_record(self.headers)?;
+                wtr.write_record(rec)?;
+                self.inner.entry(part).or_insert((wtr, 0, 0));
+            }
+        };
+        Ok(())
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        for i in self.inner.values_mut() {
+            i.0.flush()?;
+        }
+        Ok(())
+    }
+}
 
 #[derive(Clap)]
 #[clap(name = crate_name!(), version = crate_version!(), author = crate_authors!(), about = crate_description!())]
@@ -21,16 +95,22 @@ type Row = BTreeMap<String, String>;
 struct Opts {
     #[clap(short, about = "Output directory.")]
     outdir: String,
-    #[clap(long, about = "Split on which header.")]
-    on: String,
+    #[clap(long, about = "Split on header.")]
+    header: String,
+    #[clap(long, about = "Split on row count.")]
+    limit: Option<u32>,
     #[clap(about = "Input file path.")]
     input: String,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), io::Error> {
     let opts = Opts::parse();
     let input = PathBuf::from(&opts.input);
-    let prefix: &str = input.file_stem().unwrap().to_str().unwrap();
+    let prefix: &str = &format!(
+        "{}/{}",
+        opts.outdir,
+        input.file_stem().unwrap().to_str().unwrap()
+    );
     let ext: &str = input
         .extension()
         .unwrap_or(OsStr::new("tsv"))
@@ -38,31 +118,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
     let mut reader = CsvReaderBuilder::new()
         .delimiter(b'\t')
-        .buffer_capacity(1 << 20)
+        .buffer_capacity(BUFFER)
         .has_headers(true)
         .from_path(&opts.input)?;
-    let mut writers: HashMap<String, CsvWriter<File>> = HashMap::new();
     let headers = reader.headers()?.to_owned();
+    let mut writers = WriterGroup::new(prefix, ext, &headers, opts.limit);
     for rec in reader.deserialize() {
         let rec: Row = rec?;
         let part: String = rec
-            .get(&opts.on)
-            .expect(&format!("No header named {}.", &opts.on))
+            .get(&opts.header)
+            .expect(&format!("No header named {}.", &opts.header))
             .to_owned();
-        let writer = writers.entry(part.clone()).or_insert_with(|| {
-            let mut nw = CsvWriterBuilder::new()
-                .delimiter(b'\t')
-                .has_headers(true)
-                .buffer_capacity(1 << 22)
-                .from_path(format!("{}/{}.{}.{}", &opts.outdir, prefix, part, ext))
-                .unwrap();
-            nw.write_record(&headers).unwrap();
-            nw
-        });
-        writer.write_record(headers.iter().map(|v| &rec[v]).collect::<Vec<&String>>())?;
+        let recw = headers.iter().map(|v| &rec[v]).collect::<Vec<&String>>();
+        writers.write(part, &recw)?;
     }
-    for i in writers.values_mut() {
-        i.flush()?;
-    }
-    Ok(())
+    writers.flush().into()
 }
