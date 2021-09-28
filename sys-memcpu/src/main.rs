0f39093d5ab8afc34009f5fd001d86fd343dab85
slyo <sean.lyo@outlook.com>
2021-09-28 12:34:02 +0800

    sys-memcpu: Add time stamp.

--- sys-memcpu/src/main.rs
+++ sys-memcpu/src/main.rs
@@ -1,24 +1,42 @@
+extern crate env_logger;
+extern crate humantime;
+extern crate log;
 extern crate sysinfo;
 
 use std::error::Error;
+use std::io::Write;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
+use env_logger::Env;
+use log::warn;
 use sysinfo::{ProcessorExt, RefreshKind, System, SystemExt};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("warn"))
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} - {}",
+                humantime::format_rfc3339_seconds(
+                    SystemTime::now() + Duration::from_secs(8 * 3600)
+                ),
+                record.args()
+            )
+        })
+        .init();
     let refresh = RefreshKind::new();
     refresh.with_cpu();
     refresh.with_memory();
     let mut sys = System::new_with_specifics(refresh);
     sys.refresh_all();
-    let totalnt = sys.physical_core_count().unwrap_or(0);
+    let totalcpu = sys.processors().len();
     let totalmem = sys.total_memory();
     loop {
-        println!(
-            "cpu: {:.2}%*{}nt, memory: {}/{}KB.",
+        warn!(
+            "cpu: {:.2}%*{}, memory: {}/{}KB.",
             sys.global_processor_info().cpu_usage(),
-            totalnt,
+            totalcpu,
             sys.used_memory(),
             totalmem,
         );
